c07c9927e4671622dd8dc525f669876108fc2dda
Reginald <reginaldlai1996@gmail.com>
2025-11-03 14:38:14 +0800

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -40,6 +40,15 @@ class Controller extends BaseController
             $remove_worker_id = $sale->{$role . '_remove_worker_id'};
     
             if ($worker_id > 0) {
+                // Initialize worker if not exists (for deleted/inactive users)
+                if (!isset($worker[$worker_id])) {
+                    $worker[$worker_id] = [
+                        'total' => 0,
+                        'total_remove_commission' => 0,
+                        'full_total' => 0
+                    ];
+                }
+                
                 $product = Product::find($product_id);
                 $commission = $product->{$role} ?? 0;
     
@@ -49,6 +58,15 @@ class Controller extends BaseController
             }
     
             if ($remove_worker_id > 0) {
+                // Initialize worker if not exists (for deleted/inactive users)
+                if (!isset($worker[$remove_worker_id])) {
+                    $worker[$remove_worker_id] = [
+                        'total' => 0,
+                        'total_remove_commission' => 0,
+                        'full_total' => 0
+                    ];
+                }
+                
                 $remove_commission = $tintRemoveSetting->{$role} ?? 0;
                 $all_total_remove_commission += $remove_commission;
                 $worker[$remove_worker_id]['total_remove_commission'] += $remove_commission;
@@ -74,12 +92,30 @@ class Controller extends BaseController
             ];
         }
 
-
         foreach($item_sale->workers as $work){
+            // Initialize worker if not exists (for deleted users)
+            if (!isset($worker[$work->worker_id])) {
+                $worker[$work->worker_id] = [
+                    'total_sales_commission' => 0,
+                    'total_work_commission' => 0,
+                    'total_commission' => 0
+                ];
+            }
+            
             $worker[$work->worker_id]['total_work_commission'] += $work->worker_commission;
             $worker[$work->worker_id]['total_commission'] += $work->worker_commission;
         }
+        
         foreach($item_sale->salePersons as $sale){
+            // Initialize worker if not exists (for deleted users)
+            if (!isset($worker[$sale->worker_id])) {
+                $worker[$sale->worker_id] = [
+                    'total_sales_commission' => 0,
+                    'total_work_commission' => 0,
+                    'total_commission' => 0
+                ];
+            }
+            
             $worker[$sale->worker_id]['total_sales_commission'] += $sale->worker_commission;
             $worker[$sale->worker_id]['total_commission'] += $sale->worker_commission;
         }
@@ -87,6 +123,5 @@ class Controller extends BaseController
         return [
             'worker' => $worker,
         ];
-    
     }
 }
